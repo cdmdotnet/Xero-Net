3e1933e80df8db4306661d949f73d1573d3f2712
Grover <grover@results.com>
2018-04-13 10:03:43 +1200

    Added a separate copy as a separate nuget-able package incase the pull request is accepted.

---
+++ Xero.Api.Notes/Common/Extensions.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Xero.Api.Core.Endpoints;
+using Xero.Api.Core.Endpoints.Base;
+using Xero.Api.Core.Model;
+using Xero.Api.Infrastructure.Interfaces;
+
+namespace Xero.Api.Common
+{
+    public static class HistoryExtensions
+    {
+        /// <summary>
+        /// Get history/notes associated/linked/attached to the <paramref name="source"/> 
+        /// as per https://developer.xero.com/documentation/api/history-and-notes.
+        /// </summary>
+        /// <param name="endpoint">The <see cref="IXeroReadEndpoint{T,TResult,TResponse}">endpoint</see> to leverage to communicate with the Xero API.</param>
+        /// <param name="source">The item/object that you want to get history for.</param>
+        /// <returns>History/note information.</returns>
+        public static IEnumerable<History> GetHistory<T, TResult, TResponse>(this IXeroReadEndpoint<T, TResult, TResponse> endpoint, TResult source)
+            where T : XeroReadEndpoint<T, TResult, TResponse>
+            where TResponse : IXeroResponse<TResult>, new()
+            where TResult : IHasId
+        {
+            return GetHistory((T) endpoint, source);
+        }
+
+        /// <summary>
+        /// Get history/notes associated/linked/attached to the <paramref name="source"/> 
+        /// as per https://developer.xero.com/documentation/api/history-and-notes.
+        /// </summary>
+        /// <param name="endpoint">The <see cref="XeroReadEndpoint{T,TResult,TResponse}">endpoint</see> to leverage to communicate with the Xero API.</param>
+        /// <param name="source">The item/object that you want to get history for.</param>
+        /// <returns>History/note information.</returns>
+        public static IEnumerable<History> GetHistory<T, TResult, TResponse>(this XeroReadEndpoint<T, TResult, TResponse> endpoint, TResult source)
+            where T : XeroReadEndpoint<T, TResult, TResponse>
+            where TResponse : IXeroResponse<TResult>, new()
+            where TResult : IHasId
+        {
+            return new HistoryEndpoint(endpoint.Client).Get(endpoint, source);
+        }
+
+        /// <summary>
+        /// Add/link/associate a note to the <paramref name="source"/>
+        /// as per https://developer.xero.com/documentation/api/history-and-notes.
+        /// </summary>
+        /// <param name="endpoint">The <see cref="IXeroReadEndpoint{T,TResult,TResponse}">endpoint</see> to leverage to communicate with the Xero API.</param>
+        /// <param name="source">The item/object that you want to get history for.</param>
+        /// <param name="noteContent">The content of the note to add/link/associate.</param>
+        /// <returns>The created history/note information.</returns>
+        public static History AddNote<T, TResult, TRequest, TResponse>(this IXeroCreateEndpoint<T, TResult, TRequest, TResponse> endpoint, TResult source, string noteContent)
+            where T : XeroReadEndpoint<T, TResult, TResponse>
+            where TResponse : IXeroResponse<TResult>, new()
+            where TRequest : IXeroRequest<TResult>, new()
+            where TResult : IHasId
+        {
+            return AddNote((XeroCreateEndpoint<T, TResult, TRequest, TResponse>)endpoint, source, noteContent);
+        }
+
+        /// <summary>
+        /// Add/link/associate a note to the <paramref name="source"/>
+        /// as per https://developer.xero.com/documentation/api/history-and-notes.
+        /// </summary>
+        /// <param name="endpoint">The <see cref="XeroReadEndpoint{T,TResult,TResponse}">endpoint</see> to leverage to communicate with the Xero API.</param>
+        /// <param name="source">The item/object that you want to get history for.</param>
+        /// <param name="noteContent">The content of the note to add/link/associate.</param>
+        /// <returns>The created history/note information.</returns>
+        public static History AddNote<T, TResult, TRequest, TResponse>(this XeroCreateEndpoint<T, TResult, TRequest, TResponse> endpoint, TResult source, string noteContent)
+            where T : XeroReadEndpoint<T, TResult, TResponse>
+            where TResponse : IXeroResponse<TResult>, new()
+            where TRequest : IXeroRequest<TResult>, new()
+            where TResult : IHasId
+        {
+            return new HistoryEndpoint(endpoint.Client).Put(endpoint, source, noteContent);
+        }
+    }
+}
